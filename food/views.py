fc4d241782824073bfe0c92f346084010dd67712
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-08-16 11:35:33 +0300

    views index updated. Template 'index.html' added.

--- food/views.py
+++ food/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Hello World!')
+    item_list = Item.objects.all()
+    # template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list
+    }
+    return render(request, 'food/index.html', context)
 
 
 def item(request):
