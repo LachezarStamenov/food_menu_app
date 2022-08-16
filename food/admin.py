4148bdb0522105c056abfded7171a4404fe3964c
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-08-16 14:41:32 +0300

    nav bar added in index.html file

--- food/admin.py
+++ food/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Item
 
-admin.site.register(Item)
+# admin.site.register(Item)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item_name')
