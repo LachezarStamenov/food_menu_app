8eff81364894e6c33963258df23cd25b36edb2d7
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-08-17 10:14:59 +0300

    adding image field to food models

--- food/models.py
+++ food/models.py
@@ -11,4 +11,8 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://www.happyeater.com/images/default-food-image.jpg"
+    )
 
