b33c62eb6b8c725530cce16aedc283a3d08a932d
Lachezar Stamenov <lachezar.stamenov@abv.bg>
2022-08-19 12:25:02 +0300

    form for adding items added

--- food/urls.py
+++ food/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 app_name = 'food'
 urlpatterns = [
+    # /food/
     path('', views.index, name='index'),
+    # /food1
     path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
-
+    # add items
+    path('add', views.create_item, name='create_item'),
 ]
